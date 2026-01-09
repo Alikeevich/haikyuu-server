fbb80eb1d18ca11c7d170237e6bd3c1875962e00
Alikeevich <alikkenzheb@gmail.com>
2026-01-09 22:09:01 +0500

    Initial server commit

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const io = new Server(server, {
     cors: { 
         origin: [
             "http://localhost:5173",
-            "https://ваш-домен.vercel.app"  // Добавите после деплоя на Vercel
+            "haikyuu-client.vercel.app"  // Добавите после деплоя на Vercel
         ], 
         methods: ["GET", "POST"],
         credentials: true
